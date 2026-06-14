b2902548b75e21d50ad95955262461e3d7de6456
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 12:57:37 +0900

    Add files via upload

---
+++ 50p_elementat/50p_elementat/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, -1, 2, -2, 3, -3 };
+        Console.WriteLine(array.Where(c => c < 0).ElementAt(1));
+    }
+}
